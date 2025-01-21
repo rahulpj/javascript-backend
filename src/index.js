633f9d3b5605968e0ebcee948ac67b59a47acb12
Rahul Panjwani <rahulpanjwani2003@gmail.com>
2025-01-21 10:48:23 +0530

    configured routes and controller for user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
-dotenv.config({ path: "./env" });
+dotenv.config();
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
-const asynHandler = (requestHandler) => {
-  (req, res, next) => {
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
